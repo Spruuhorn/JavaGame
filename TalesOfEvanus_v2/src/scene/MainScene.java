4566e5bb227b4c371d749c525742feabbd46e425
Jason <Jason@EdshotMachine.home>
2019-05-19 23:16:08 -0400

    added Tales of Evanus
    this is the second iteration of the second iteration of Tales of Evanus

---
+++ TalesOfEvanus_v2/src/scene/MainScene.java
@@ -0,0 +1,39 @@
+package scene;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class MainScene extends BasicGameState {
+	
+	public MainScene (int state) {
+		// Nothing to do here.
+	}
+	
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		
+		//gc.setShowFPS(false);
+		
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int time) throws SlickException {
+		
+	}
+
+	@Override
+	public int getID() {
+		
+		return 0;
+		
+	}
+
+}
